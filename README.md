f2cf47a3bddc7c457a1c0729ce4da1c5eb3b7d30
Brian Dong <brian.dong@nxp.com>
2017-05-18 14:44:23 +0800

    adding unit test and Rakefile support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Introduction
 
-This simple script helps create an executable ruby script.
+This simple script helps create an executable ruby script, along with the supporting unit test script and Rakefile.
 
-The idea is from book "Practical Ruby for System Administration".
+The idea of this script is from book "Practical Ruby for System Administration" by Andre Ben Hamou.
 
--- newScript.rb
+++ newScript.rb
@@ -3,12 +3,36 @@
 path = ARGV[0]
 fail "specify filename to create" unless path
 
+path_list = path.split('/')
+dir = path_list[0..-2].join('/')
+
+# sample script
 File.open(path, "w") do |f|
 	f.puts "#!/usr/bin/env ruby -w"
+	f.puts "\n# Definition of a sample user class"
+	f.puts "class User"
+	f.puts "\n    # Name of user"
+	f.puts "    attr_accessor :name"
+	f.puts "\n    # What does the user say"
+	f.puts "    def speak"
+	f.puts "        reture 'HelloWorld!'"
+	f.puts "    end"
+	f.puts "\nend #class User"
 	f.puts "\n# Only run the following code when this file is the main file being run"
 	f.puts "if __FILE__ == $0"
-	f.puts "end"
+	f.puts "end #if"
 end
 
 File.chmod(0755, path)
 system "open", path
+
+# unit test script
+test_dir = dir+"/test"
+Dir.mkdir test_dir
+
+test_path = test_dir+"/tc_"+path_list[-1]
+File.open(test_path, "w") do |f|
+	f.puts "#!/usr/bin/env ruby -w"
+end
+
+# Rakefile
