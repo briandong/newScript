c7b98c979c7f5d3b2eb29e6ff341024c08235919
bo.dong <bo.dong@enflame-tech.com>
2020-09-07 15:52:47 +0800

    add optparse

--- newScript.py
+++ newScript.py
@@ -1,67 +1,97 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-' a script to generate new python project '
+' a script to create new python project '
 
 __author__ = "Bo DONG"
 
 import sys, os
+import optparse
+
 
 def main():
-    if len(sys.argv) == 1:
-        print("Please specify the project dir")
+    usage = "usage: python %prog [options]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-d', '--dir',
+        action="store", dest="target_dir",
+        help="target dir", default=None)
+    options, args = parser.parse_args()
+
+    print("Arguments:", args)
+    print("Target dir:", options.target_dir)
+
+    path = options.target_dir
+    if os.path.isdir(path):
+        print("Dir '{}' already exists. Please specify a new dir".format(path))
     else:
-        path = sys.argv[1]
-        if os.path.isdir(path):
-            print("Dir '{}' already exists. Please specify a new dir".format(path))
-        else:
-            # Get project name
-            name = path.split('/')[-1]
+        # get project name
+        name = path.split('/')[-1]
 
-            # Create dirs
-            print("Creating dir: {}".format(path))
-            os.makedirs(path+"/"+name)
-            os.makedirs(path+"/tests")
-            os.makedirs(path+"/docs")
+        # ****************
+        # create dirs
+        # ****************
+        print("Creating dir: {}".format(path))
+        os.makedirs(path+"/"+name)
+        os.makedirs(path+"/tests")
+        os.makedirs(path+"/docs")
 
-            # Create files
-            header = """#!/usr/bin/env python
+        # ****************
+        # create files
+        # ****************
+        header = """#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-' a script to do some work '
-
-__author__ = "J Smith"
+' a script to do work '
+__author__ = "Bo DONG"
 
 import sys, os
 """
-            # Create script
-            body = """
+        footer = """
+# only run the following code when this file is the main file being run
+if __name__=='__main__':
+    main()
+
+"""
+
+        # create script
+        body = """
+import optparse
+
+
 def func():
     return "Hello World!"
 
 def main():
+    usage = "usage: python %prog [options]"
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-d', '--dir',
+        action="store", dest="target_dir",
+        help="target dir", default=None)
+    options, args = parser.parse_args()
+
+    print("Arguments:", args)
+    print("Target dir:", options.target_dir)
+
     print(func())
 
-# Only run the following code when this file is the main file being run
-if __name__=='__main__':
-    main()
 """
-            with open(path+"/"+name+"/"+name+".py", "w") as f:
-                f.write(header)
-                f.write(body)
-            
-            # Create context
-            context_body = """
+        with open(path+"/"+name+"/"+name+".py", "w") as f:
+            f.write(header)
+            f.write(body)
+            f.write(footer)
+        
+        # create context
+        context_body = """
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '{}')))
 
 import {}
 """.format(name, name)
-            with open(path+"/tests/context.py", "w") as f:
-                f.write(header)
-                f.write(context_body)
+        with open(path+"/tests/context.py", "w") as f:
+            f.write(header)
+            f.write(context_body)
 
-            # Create tests
-            test_body = """import unittest
+        # create tests
+        test_body = """import unittest
 
 from context import {}
 
@@ -70,12 +100,12 @@ class Test_func(unittest.TestCase):
         self.assertEqual({}.func(), "Hello World!")
 """.format(name, name)
 
-            with open(path+"/tests/test_"+name+".py", "w") as f:
-                f.write(header)
-                f.write(test_body)
+        with open(path+"/tests/test_"+name+".py", "w") as f:
+            f.write(header)
+            f.write(test_body)
 
-            # Create Makefile
-            make_body = """
+        # create Makefile
+        make_body = """
 init:
     pip install -U pytest
 
@@ -84,25 +114,26 @@ test:
 
 PHONY: init test
 """
-            with open(path+"/Makefile", "w") as f:
-                f.write(make_body)
+        with open(path+"/Makefile", "w") as f:
+            f.write(make_body)
 
-            # Create README
-            read_body = """
+        # create README and docs
+        readme_body = """
 [TOC]
 
 # Introduction
 
-This script can do some work
+This script can do work
 
 """
-            with open(path+"/README.md", "w") as f:
-                f.write(read_body)
+        with open(path+"/README.md", "w") as f:
+            f.write(readme_body)
 
-            # Create docs
-            with open(path+"/docs/"+name+".md", "w") as f:
-                pass
+        # Create docs
+        with open(path+"/docs/"+name+".md", "w") as f:
+            f.write(readme_body)
 
-# Only run the following code when this file is the main file being run
+
+# only run the following code when this file is the main file being run
 if __name__=='__main__':
     main()
